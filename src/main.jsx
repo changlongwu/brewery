d9fc4e870dce8f8fb7eb11535ca0dd88e539cb16
changlongwu <changlong_wu@berkeley.edu>
2024-07-07 21:45:13 -0700

    not use route

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,15 @@ import Layout from './routes/Layout.jsx';
 import BreweryView from './routes/BreweryView.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <BrowserRouter>
-  <Routes>
-    <Route path='/brewery/' element={<Layout/>}>
-      <Route index={true} element={<App/>} />
-      <Route index={false} path='/breweryDetail/:id' element={<BreweryView />} />
-    </Route>
-  </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // <BrowserRouter>
+  // <Routes>
+  //   <Route path='/brewery/' element={<Layout/>}>
+  //     <Route index={true} element={<App/>} />
+  //     <Route index={false} path='/breweryDetail/:id' element={<BreweryView />} />
+  //   </Route>
+  // </Routes>
+  // </BrowserRouter>
 )
